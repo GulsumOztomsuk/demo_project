f6e013c5d1d7acc3cb8632c40f109292771321ac
w10 <glsmh.16@gmail.com>
2023-03-09 16:10:03 +0300

    demo project

---
+++ src/test/java/cbt/CukesRunner/CukesRunner.java
@@ -0,0 +1,18 @@
+package cbt.CukesRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/cbt"
+)
+public class CukesRunner {
+
+}
